8de0c86fea0c18a6e8b053965579cb73850283e6
John Griffith <john.griffith@live.co.uk>
2017-03-04 13:48:51 +0000

    Guessing game

---
+++ 2_Guessing_Game/src/main.rs
@@ -0,0 +1,20 @@
+extern crate rand;
+
+
+use std::io;
+use rand::Rng;
+
+fn main() {
+
+    println!("Guess the number!");
+    println!("Please input your guess: ");
+
+    let secret_num = rand::thread_rng().gen_range(1,101);
+
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line :(");
+
+    println!("You guessed: {}", guess);
+    println!("The secret number is {}.",secret_num);
+}

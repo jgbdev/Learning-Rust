37677badc42c6ae07063cf56a9e041e81d355948
John Griffith <john.griffith@live.co.uk>
2017-03-05 12:50:00 +0000

    Functions, etc

--- 3_Syntax_Semantics/src/main.rs
+++ 3_Syntax_Semantics/src/main.rs
@@ -24,11 +24,45 @@ fn main() {
 
     //Expressions and Statements
 
-    //Invalid: let x = (let y = 5); 
+    //Invalid: let x = (let y = 5);
+    //TODO: Flesh out
+
+    // Divergence
+
+    //diverges()  Calling this will not return anything, due to ! ret type
+    //Similar to how println! doesn't return anything
+
+    //We can assign a divergence to any type
+    let x: i32 = diverges();
+    let x: String = diverges();
+
+    // Function Pointers
+
+    // We can create variable bindings which point to functions
+
+    // Without type inference:
+    let f: fn(i32) -> i32 = plus_one;
+
+    // With type inference:
+    let f = plus_one;
+
+    let six = f(5);
 
 
     println!("Finished!");
 }
+
+
+fn diverges() -> ! {
+    panic!("This function never returns!");
+}
+
+//fn early_return(x: i32) -> i32 {
+    //return x + 1; // Return works
+
+    //let y: i32 = 10; Becomes unreachable
+//}
+
 
 
 fn print_number(x: i32){
